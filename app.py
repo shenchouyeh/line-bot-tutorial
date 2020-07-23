f29461a0899d0dde712118442959590655b416c3
Jacky Yeh <shenchou@gmail.com>
2020-07-24 01:20:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,15 @@ def callback():
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
-
+#def handle_message(event):
+#    message = TextSendMessage(text=event.message.text)
+#    line_bot_api.reply_message(event.reply_token, message)
+def echo(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )    
+    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
