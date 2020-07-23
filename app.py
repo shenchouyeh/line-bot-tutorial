748dc4689509815f9dd79598cace18263115a77e
Jacky Yeh <shenchou@gmail.com>
2020-07-24 01:41:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,30 +22,42 @@ def home():
     return render_template("home.html")
 # 增加的這段放在上面
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/", methods=['POST'])
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
-    # handle webhook body
+    app.logger.info("Request body: " + body)
+    
     try:
+        print(body, signature)
         handler.handle(body, signature)
+        
     except InvalidSignatureError:
-       abort(400)
+        abort(400)
 
     return 'OK'
 
-
+# 學你說話
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    print(msg)
-    msg = msg.encode('utf-8')
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+def pretty_echo(event):
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        
+        # Phoebe 愛唱歌
+        pretty_note = '♫♪♬'
+        pretty_text = ''
+        
+        for i in event.message.text:
+        
+            pretty_text += i
+            pretty_text += random.choice(pretty_note)
+    
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=pretty_text)
+        )
     
 import os
 if __name__ == "__main__":
