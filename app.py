fc6cb67c732a8d609eb6f3309e503f7fcac63f2f
Jacky Yeh <shenchou@gmail.com>
2020-07-24 13:06:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -161,5 +161,7 @@ def handle_location_message(event):
         )
     )
  
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
